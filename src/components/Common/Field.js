73a3b930a622622ba271a9f03e16aaa1bd4f95c1
Tomi-star <Tomisin.akinfemiwa@gmail.com>
2020-07-08 03:03:58 +0100

    Validation Added to contacts

--- src/components/Common/Field.js
+++ src/components/Common/Field.js
@@ -3,31 +3,41 @@ import React from 'react';
 class Field extends React.Component{
     render(){
         return (
-          <div className="form-group">
+          <div>
             {this.props.elementName === "input" ? (
-              <input
-                className="form-control"
-                id={this.props.name}
-                type={this.props.type}
-                placeholder={this.props.placeholder}
-                required="required"
-                data-validation-required-message="Please enter your name."
-                name={this.props.name}
-                onChange={this.props.onChange}
-              />
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  id={this.props.name}
+                  type={this.props.type}
+                  placeholder={this.props.placeholder}
+                  required="required"
+                  data-validation-required-message={this.props.validMessage}
+                  name={this.props.name}
+                  onChange={this.props.onChange}
+                  onBlur={this.props.onBlur}
+                />
+              </div>
             ) : (
-              <textarea
-                className="form-control"
-                id={this.props.name}
-                placeholder={this.props.placeholder}
-                required="required"
-                data-validation-required-message="Please enter a message."
-                name={this.props.name}
-                onChange={this.props.onChange}
-              />
+              <div className="form-group form-group-textarea mb-md-0">
+                <textarea
+                  className="form-control "
+                  id={this.props.name}
+                  placeholder={this.props.placeholder}
+                  required="required"
+                  data-validation-required-message={this.props.validMessage}
+                  name={this.props.name}
+                  onChange={this.props.onChange}
+                  onBlur={this.props.onBlur}
+                />
+              </div>
             )}
 
-            <p className="help-block text-danger"></p>
+            <p className="help-block text-danger">
+              {(this.props.touched && this.props.errors) &&
+                <span> {this.props.errors}</span>
+              }
+            </p>
           </div>
         );
     }
--- src/components/Pages/Contacts.js
+++ src/components/Pages/Contacts.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import Field from '../Common/Field'
-//import {withFormik} from 'formik';
+import React from "react";
+import Field from "../Common/Field";
+import { withFormik } from "formik";
+import * as Yup from "yup";
 
 const fields = {
-  section:[
+  sections: [
     [
-      { name: "name", elementName: "Input", type: "text", placeholder: "Your Name" },
-      { name: "email", elementName: "Input", type: "email", placeholder: "Your Email" },
-      { name: "phone", elementName: "Input", type: "text", placeholder: "Your Phone Number" }
+      {
+        name: "Name",
+        elementName: "input",
+        type: "text",
+        placeholder: "Name",
+        validMessage: "Please enter your Name",
+      },
+
+      {
+        name: "email",
+        elementName: "input",
+        type: "email",
+        placeholder: "Email",
+        validMessage: "Please enter your Email",
+      },
+
+      {
+        name: "phone",
+        elementName: "input",
+        type: "tel",
+        placeholder: "Phone number",
+        validMessage: "Please enter your Phone number",
+      },
     ],
     [
-      { name: "message", elementName: "textarea", type: "text", placeholder: "Your Message" }
-    ]
-  ]
-}
-
-
+      {
+        name: "message",
+        type: "text",
+        placeholder: "Message",
+        elementName: "textarea",
+        validMessage: "Please enter your Message",
+      },
+    ],
+  ],
+};
 
 class Contacts extends React.Component {
-
-  submitForm = (e) => {
-    alert("Submitted,Thank you.");
-  };
-  
   render() {
     return (
       <section className="page-section" id="contact">
@@ -33,52 +53,74 @@ class Contacts extends React.Component {
               Lorem ipsum dolor sit amet consectetur.
             </h3>
           </div>
-          <div className="row">
-            <div className="col-lg-12">
-              <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                <div className="row">
-                  {fields.section.map((section, sectionIndex) => {
-                    console.log(
-                      "Rendering section",
-                      sectionIndex,
-                      "with",
-                      section
-                    );
-                    return (
-                      <div className="col-md-6" key={sectionIndex}>
-                        {section.map((field, i) => {
-                          return (
-                            <Field
-                              {...field}
-                              key={i}
-                              valus={this.props.values[field.name]}
-                              name={field.name}
-                              onchange={this.props.handleChange}
-                            />
-                          );
-                        })}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="text-center">
-                  <div id="success"></div>
-                  <button
-                    className="btn btn-primary btn-xl text-uppercase"
-                    id="sendMessageButton"
-                    type="submit"
-                    onClick={(e) => this.submitForm(e)}
-                  >
-                    Send Message
-                  </button>
-                </div>
-              </form>
+          <form
+            id="contactForm"
+            name="sentMessage"
+            novalidate="novalidate"
+            onSubmit={this.props.handleSubmit}
+          >
+            <div className="row align-items-stretch mb-5">
+              {fields.sections.map((section, sectionIndex) => {
+                console.log("rendering section", sectionIndex, "with", section);
+                return (
+                  <div className="col-md-6" key={sectionIndex}>
+                    {section.map((field, i) => {
+                      return (
+                        <Field
+                          {...field}
+                          key={i}
+                          values={this.props.values[field.name]}
+                          name={field.name}
+                          onChange={this.props.handleChange}
+                          onBlur={this.props.handleBlur}
+                          touched={this.props.touched[field.name]}
+                          errors={this.props.errors[field.name]}
+                        />
+                      );
+                    })}
+                  </div>
+                );
+              })}
             </div>
-          </div>
+            <div className="text-center">
+              <div id="success"></div>
+              <button
+                className="btn btn-primary btn-xl text-uppercase"
+                type="submit"
+              >
+                Send Message
+              </button>
+            </div>
+          </form>
         </div>
       </section>
     );
   }
 }
 
-export default Contacts;
+export default withFormik({
+  mapPropsToValues: () => ({
+    Name: "",
+    email: "",
+    phone: "",
+    message: "",
+  }),
+  validationSchema: Yup.object().shape({
+    Name: Yup.string()
+      .min(3, "Come on bro your name is longer than that")
+      .required("you must give us your name."),
+    email: Yup.string()
+      .email("You need to give us a valid emial")
+      .required("we need your email"),
+    phone: Yup.string()
+      .min(10, "Please provide your 10 digit phone number")
+      .max(16, "Your phone number is too long")
+      .required("pleasse provide your phone number"),
+    message: Yup.string().min(100, "Please provide more details")
+      .required('Please provide a message')
+  }),
+
+  handleSubmit: (values, { setSubmitting }) => {
+    alert("You've submitted the form", JSON.stringify(values));
+  },
+})(Contacts);
