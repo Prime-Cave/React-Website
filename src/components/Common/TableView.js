55357bd184c1a2f3ad7061e76d9e13d5895ed992
Tomi-star <Tomisin.akinfemiwa@gmail.com>
2020-07-18 23:23:41 +0100

    Still on this same error o

--- src/components/Common/TableView.js
+++ src/components/Common/TableView.js
@@ -6,7 +6,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
-import TableContainer from '@material-ui/core/TableContainer';
 
 
 
@@ -14,12 +13,12 @@ class TableView extends React.Component{
     render(){
             const {rows, columns } = this.props;
         return (
-          <TableContainer component={Paper}>
+          <Paper>
             <Table>
               <TableHead>
                 <TableRow>
-                  {columns
-                    ? columns.map((col, i) => {
+                  {columns ? 
+                        columns.map((col, i) => {
                         return <TableCell key={i}>{col}</TableCell>;
                       })
                     : null}
@@ -27,8 +26,8 @@ class TableView extends React.Component{
               </TableHead>
 
               <TableBody>
-                {rows
-                  ? rows.map((row, i) => {
+                {rows ? 
+                    rows.map((row, i) => {
                       return columns.map((col, colIndex) => {
                         return <TableCell>{row[col.name]}</TableCell>;
                       });
@@ -36,7 +35,7 @@ class TableView extends React.Component{
                   : null}
               </TableBody>
             </Table>
-          </TableContainer>
+          </Paper>
         );
     }
 }
